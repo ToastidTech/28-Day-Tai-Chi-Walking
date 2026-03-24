c79a5ae20289f46fb397a1a99be8c25e0a033fda
ToastidTech <toastidtechllc@gmail.com>
2026-03-24 18:00:20 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-const CACHE = 'taichi-v1';
+const CACHE = 'taichi-v2';
 const ASSETS = [
   '/28-Day-Tai-Chi-Walking/',
   '/28-Day-Tai-Chi-Walking/index.html',
+  '/28-Day-Tai-Chi-Walking/manifest.json',
   '/28-Day-Tai-Chi-Walking/Logo.png',
+  '/28-Day-Tai-Chi-Walking/icon-192.png',
+  '/28-Day-Tai-Chi-Walking/icon-512.png',
 ];
 
+// Install: cache all assets
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting(); // activate immediately
 });
 
+// Activate: delete old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim(); // take control of all open tabs
+});
+
+// Fetch: serve from cache, fall back to network
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
